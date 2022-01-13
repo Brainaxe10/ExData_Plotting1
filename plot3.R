e3e8a431f09b2331da976a469da17eae0fdc7443
Brainaxe10 <shanthang7@gmail.com>
2022-01-13 15:28:49 -0500

    correct plot3.R

--- plot3.R
+++ plot3.R
@@ -1,14 +1,19 @@
 dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE, dec = ".")
 subdat <- dat[dat$Date %in% c("1/2/2007", "2/2/2007"),]
 globalActivePower <- as.numeric(subdat$Global_active_power)
-datetime <- strptime(paste(subdat$Date, subdat$Time, sep = " "), "%d/%m%/%Y %H:%M:%S")
-submeter1 <- as.numeric(subdat$Sub_metering_1)
-submeter2 <- as.numeric(subdat$Sub_metering_2)
-submeter3 <- as.numeric(subdat$Sub_metering_3)
 
-png("plot3.png")
-plot(datetime, submeter1, type = "l", ylab = "Energy Submetering")
-lines(datetime, submeter2, type = "l", col = "red")
-lines(datetime, submeter3, type = "l", col = "blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
+subdat$Date <- as.Date(subdat$Date, format="%d/%m/%Y")
+subdat$Time <- strptime(subdat$Time, format="%H:%M:%S")
+subdat[1:1440,"Time"] <- format(subdat[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subdat[1441:2880,"Time"] <- format(subdat[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+plot(subdat$Time,subdat$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
+with(subdat,lines(Time,as.numeric(as.character(Sub_metering_1))))
+with(subdat,lines(Time,as.numeric(as.character(Sub_metering_2)),col="red"))
+with(subdat,lines(Time,as.numeric(as.character(Sub_metering_3)),col="blue"))
+legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+title(main = "Energy sub-metering")
+
+dev.copy(png, filename = "plot3.png")
 dev.off()
